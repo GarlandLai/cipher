5e0f26fd4c663f3f2db87b00394084d7c5d4afed
Garland Lai <garlandlai24@gmail.com>
2019-06-03 21:24:12 -0700

    add the next objective to call previous functions (does not work)

--- js/script.js
+++ js/script.js
@@ -21,3 +21,10 @@ alert(firstLastCap);
 
 var revFirstLastCap = lastCap + noFirstLast + firstCap;
 alert(revFirstLastCap);
+
+//Create a third function that calls the other two functions you've just created. For example, if I enter the sentence "I am a sentence", it should enter your first function (which should output "IE"), then call your second function, passing in the output value from the previous. It would return "EI" to me.
+var previousTwo = function(){
+  var firstLastCap;
+  var revFirstLastCap;
+}
+alert (previousTwo);
